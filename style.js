7c806f34736f75de2b0f9496d8b3cbdb114e2557
Max Yeo <mzwyeo@gmail.com>
2020-09-02 10:18:22 -0700

    redid splash and about

---
+++ style.js
@@ -0,0 +1,23 @@
+let kpop = [
+	"Ne6yxGVJQOo", // hylt
+	"K-la5k41eD0", // all night
+	"9DpG1wA_7MU", // feel special
+	"Y3gIqq0qgdQ", // bad
+	"9ID5G-f4qo0", // move
+	"rLtxNv2pWzY", // water
+	"_kqwTVTgVDI", // fancy
+	"OXdnHmj1T6E", // boom
+	"37VGUKBtrhA", // so hot
+	"F9o9_HGES_8", // birthday
+	"yXLthJYzVzQ", // pretty u
+];
+
+// image url = https://i.ytimg.com/vi/Ne6yxGVJQOo/hqdefault.jpg
+// video url = https://www.youtube.com/watch?v=Ne6yxGVJQOo
+
+let kpopReel = document.getElementById('kpop-reel');
+_.each(kpop, function(id) {
+	let image = `https://i.ytimg.com/vi/${id}/hqdefault.jpg`;
+	let video = `https://www.youtube.com/watch?v=${id}`;
+	kpopReel.insertA
+}); 
