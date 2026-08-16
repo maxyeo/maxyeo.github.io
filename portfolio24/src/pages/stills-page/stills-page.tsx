0df5a7a66af06a8515cceb49a6ae2da424aebac9
Max Yeo <mzwyeo@gmail.com>
2026-08-16 06:36:42 -0700

    Render a single responsive layout per content page
    Stills and motion pages each built both a mobile and desktop layout
    and hid one with display: none. display: none does not stop the
    browser from fetching images or instantiating iframes, so every page
    load paid for both copies (39 images fetched twice on stills, 8
    YouTube embeds instantiated 16 times on motion). Both hidden layouts
    also reused the same DOM id, producing duplicate ids on the page.
    
    Render a single flat list per page and let CSS handle the responsive
    split instead of duplicating markup in JS:
    - motion-page uses a CSS grid (1 column mobile, 2 columns desktop)
      since videos are fixed 16:9, reproducing the old round-robin visual
      order exactly.
    - stills-page uses CSS multi-column (1 column mobile, 3 columns
      desktop) to preserve the masonry density, since photos are variable
      height.
    
    Closes #9

--- portfolio24/src/pages/stills-page/stills-page.tsx
+++ portfolio24/src/pages/stills-page/stills-page.tsx
@@ -3,50 +3,14 @@ import './stills-page.css'
 
 export function StillsPage() {
   const images: Still[] = stills;
-  const one: Still[] = [];
-  const two: Still[] = [];
-  const three: Still[] = [];
-
-  for (let i = 0; i < images.length; i++) {
-    if (i % 3 === 0) {
-      one.push(images[i]);
-    } else if (i % 3 === 1) {
-      two.push(images[i]);
-    } else {
-      three.push(images[i]);
-    }
-  }
-
-  const mobileLayout = <div id='stills-list' className='mobile-layout'>
-    <div className='stills-col'>
-      {images.map((image) => (
-        <img key={image.path} src={image.path} />
-      ))}
-    </div>
-  </div>
-
-  const desktopLayout = <div id='stills-list' className='desktop-layout'>
-    <div className='stills-col'>
-      {one.map((image) => (
-        <img key={image.path} src={image.path} />
-      ))}
-    </div>
-    <div className='stills-col'>
-      {two.map((image) => (
-        <img key={image.path} src={image.path} />
-      ))}
-    </div>
-    <div className='stills-col'>
-      {three.map((image) => (
-        <img key={image.path} src={image.path} />
-      ))}
-    </div>
-  </div>
 
   return (
     <div id='stills'>
-      { mobileLayout }
-      { desktopLayout }
+      <div id='stills-list'>
+        {images.map((image) => (
+          <img key={image.path} src={image.path} />
+        ))}
+      </div>
     </div>
   )
 }
